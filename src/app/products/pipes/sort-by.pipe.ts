3350c0695002cf6c56e31a56acdcce349f338d4c
Alberto Palacios <albert_3520@hotmail.com>
2023-10-18 10:11:07 -0600

    actualizacion tipo de orden

--- src/app/products/pipes/sort-by.pipe.ts
+++ src/app/products/pipes/sort-by.pipe.ts
@@ -14,13 +14,13 @@ export class SortByPipe implements PipeTransform {
 
       case 'name':
         // usar simbolo > mayor que para orden ascendente, y < menor que para orden descendente
-        return heroes.sort((a, b) => (a.name < b.name) ? 1 : -1);
+        return heroes.sort((a, b) => (a.name > b.name) ? 1 : -1);
 
       case 'canFly':
-        return heroes.sort((a, b) => (a.canFly < b.canFly) ? 1 : -1);
+        return heroes.sort((a, b) => (a.canFly > b.canFly) ? 1 : -1);
 
       case 'color':
-        return heroes.sort((a, b) => (a.color > b.color) ? 1 : -1);
+        return heroes.sort((a, b) => (a.color < b.color) ? 1 : -1);
 
       default:
         return heroes;
